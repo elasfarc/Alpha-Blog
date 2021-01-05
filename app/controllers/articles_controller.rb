fa894638ef8c9f1d12a317d1bea68be81ee90442
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-06 01:47:36 +0200

    Add new article feature using regular HTML forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,26 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.all
+    end
+
+    def new
         
+    end
+
+    def create
+
+        #@article = Article.new(title: params[:title], description: params[:description])
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to article_path(@article.id)
+        else 
+            pp 'error'
+            render :new
+        end
+
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
