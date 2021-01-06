c7d132b34c6505cb0792a2bf7b7936c92ec2b666
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-06 10:53:00 +0200

    Add Delete article feature & Action links for (show/edit/delete) in index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,6 +39,13 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:alert] = 'Article was deleted!'
+        redirect_to articles_path
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to:"pages#about"
-  resources :articles, only:[:index, :new, :create, :show, :edit, :update]
+  resources :articles, only:[:index, :new, :create, :show, :edit, :update, :destroy]
   
 end
