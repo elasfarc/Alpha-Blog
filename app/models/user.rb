a37ed8af95a593463ddd3e98d56bb55180874482
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-08 13:13:37 +0200

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: {minimum:6, maximum:12}
+    validates :name, 
+            presence: true,
+             uniqueness: { case_sensitive: false },
+             length: {minimum:6, maximum:12}
+
+    EMAIL_VALIDATE = /\A(\S+)@(.+)\.(\S+)\z/
+    validates :email, 
+            presence: true,
+            uniqueness: { case_sensitive: false } ,
+            length: {maximum: 100},
+            format: {with: EMAIL_VALIDATE}
     has_many :articles
 end
