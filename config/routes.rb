34db021db30f5b715b79304480bdd237f2e8105d
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-13 18:34:39 +0200

    Handle creation of a new user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to:"pages#about"
   resources :articles, only:[:index, :new, :create, :show, :edit, :update, :destroy]
-  
+  resources :users
 end
