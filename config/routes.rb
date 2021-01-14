1821e5acbc7ec21c0b9f48183c939afaa0bfc3c7
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-14 15:35:23 +0200

    Add form partial for user & edit routes file for ./signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to:"pages#about"
   resources :articles, only:[:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
