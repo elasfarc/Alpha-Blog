16536bdf5da56e407b242615f522f40b2105e70b
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-15 18:13:46 +0200

    Add routes to login in & logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :articles, only: %i[index new create show edit update destroy]
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
