0e904109931ce441f4c395342bb38d932c139fd3
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-15 17:54:38 +0200

    Add pagination
    Add pagination to users/articles listing pages & user's articles inuser's profile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,8 @@ class ArticlesController < ApplicationController
   def show; end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+
   end
 
   def new
