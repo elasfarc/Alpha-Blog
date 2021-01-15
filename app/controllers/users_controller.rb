92f026919f45ae549dd5ed1a98450c3b4c5ba890
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-16 00:12:31 +0200

    Set session once the user signup & clean code by adding  private method for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :edit]
   def new
     @user = User.new
   end
@@ -8,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "welcome #{@user.name}"
       redirect_to articles_path
     else
@@ -16,11 +18,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+   
   end
 
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:notice] = " Your user's info has been updated"
       redirect_to user_path(@user)
@@ -30,7 +32,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -42,5 +43,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])  
   end
 end
