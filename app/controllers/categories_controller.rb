2cd866178b602fdc113665bc4cc2579b40e289ee
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-19 21:54:47 +0200

    Add create a new category feature & functional test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,24 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def show; end
+
+  def index; end
+  def create
+
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = 'Category was successfully created'
+        redirect_to category_path(@category)
+      else
+        render :new
+      end
+  end
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'confirm-user-deletion', to: 'pages#confirm_password'
+  resources :categories
 end
