1dc25d44ec73ac06f663e09f9a259d720d81d583
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2021-01-20 04:08:08 +0200

    Add many to many association between article and category models

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     validates :name, presence: true, length: {maximum: 25, minimum: 3}
     validates_uniqueness_of :name
+    has_many :article_categories
+    has_many :articles, through: :article_categories
 end
